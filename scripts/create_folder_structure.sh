45d6c1bf660342166128b31d615298df2b5375cf
schumischumi <sebastian.schumacher@protonmail.com>
2023-10-02 16:05:51 +0200

    Create create_folder_structure.sh

---
+++ scripts/create_folder_structure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/docker/portainer/
+mkdir -p ~/docker/qbittorrent/
+mkdir -p ~/docker/paperless/
+mkdir -p ~/docker/jellyfin/
+mkdir -p ~/backup/restic/
+mkdir -p ~/backup/scripts/
