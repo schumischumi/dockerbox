8ff259fb5ed5ac4b5d3cd94eb4a6dcb9c18d1f19
schumischumi <sebastian.schumacher@protonmail.com>
2023-10-02 16:06:59 +0200

    Create download-blocklist.sh

---
+++ scripts/download-blocklist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+trap_handler() {
+    exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        # echo "Exit code is zero"
+        curl http://uptime-kuma.dockerbox.local/api/push/k3UrE9OU6p?status=up&msg=OK
+    else
+        # echo "Exit code is non-zero: $exit_code"
+        # Add your curl command for non-zero exit code here
+        # Example: curl -X GET http://example.com/error
+        curl http://uptime-kuma.dockerbox.local/api/push/k3UrE9OU6p?status=up&msg=FAILURE
+    fi
+}
+
+trap trap_handler EXIT
+
+blocklist_url="http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=zip"
+tmp_dir=$(mktemp -d)
+blocklist_file="/home/user/docker/qbittorrent/blocklist.p2p"
+blocklist_txt=""
+
+wget "$blocklist_url" -O "$tmp_dir/blocklist.zip"
+unzip "$tmp_dir/blocklist.zip" -d "$tmp_dir"
+blocklist_txt=$(find "$tmp_dir" -type f -name "*.txt")
+
+grep -v '^[[:space:]]*$' "$blocklist_txt"  > "$blocklist_file"
